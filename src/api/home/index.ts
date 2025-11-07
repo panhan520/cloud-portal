9358c85bdbd7c48f73f4659c9b94eb89830ee1bf
ohh <ohh@126.com>
2025-11-07 14:15:52 +0800

    feat(user): 登录字段校验修改+交互样式修改

---
+++ src/api/home/index.ts
@@ -0,0 +1,21 @@
+import getReqByProxyModule from "@/config/request";
+
+import { PROXY } from "@/config/constants";
+const userAxios = getReqByProxyModule({ proxyModule: PROXY.USER });
+
+// 查询云产品状态
+export const getCloudProductsApi = (): Promise<IPaginationResponse> => {
+  return userAxios.get("/apis/v1/cloud/products");
+};
+
+// 开通云产品
+export const openCloudProductsApi = (
+  data: any
+): Promise<IPaginationResponse> => {
+  return userAxios.put("/apis/v1/cloud/products", data);
+};
+
+// 联系销售
+export const inquiriesSalesApi = (data: any): Promise<IPaginationResponse> => {
+  return userAxios.put("/apis/v1/cloud/products/sales/inquiries", data);
+};
